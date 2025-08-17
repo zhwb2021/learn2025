2a37534a354a4df32f0227d66003a9baa78f56ad
zhwb2021 <zhwb2016@gmail.com>
2025-08-17 11:44:22 -0400

    2nd add pathVariable Commission

--- src/main/java/com/wbz/learn2025/controller/TestController.java
+++ src/main/java/com/wbz/learn2025/controller/TestController.java
@@ -1,13 +1,14 @@
 package com.wbz.learn2025.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
-    @GetMapping("/hello")
-    public String sayHello(String name) {
-        return "Hello From SpringBoot";
+    @GetMapping("/hello/{name}")
+    public String sayHello(@PathVariable String name) {
+        return "Hello From SpringBoot" + name;
     }
 }
